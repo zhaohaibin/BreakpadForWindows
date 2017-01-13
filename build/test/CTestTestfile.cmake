07a509fd188bfd7ee2b332e763758efcd5d1905c
zhaohaibin <993277531@qq.com>
2017-01-13 14:30:59 +0800

    ..

---
+++ build/test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/zhaohaibin/breakpad/mendeley/test
+# Build directory: D:/zhaohaibin/breakpad/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
